1bccf090d662cb214e829e4c964b650e131cf5aa
Isai Jesus <isaijesus02@gmail.com>
2023-03-19 14:04:18 -0600

    cart-reducers

--- src/App-IsaiJesus.js
+++ src/App-IsaiJesus.js
@@ -1,11 +1,9 @@
-import CrudApi from "./components/CrudApi";
 import ShoppingCart from "./components/ShoppingCart";
 
 function App() {
   return (
     <div className="App">
       <h1>Carrito</h1>
-      <CrudApi/>
       <hr/>
       <ShoppingCart/>
     </div>
